092b03bc0c9d7b975d6a5ac2f3cb5fa9b3af2e0d
Abdelrhman-Dev <58493502+AbdrhmanDev@users.noreply.github.com>
2026-04-01 23:59:26 +0200

    DTOs

--- GameStore.Api/DTOs.cs
+++ GameStore.Api/DTOs.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,10 +17,16 @@ public record GameDto
 }
 public record CreateGameDto
 {
+    [Required]
+    [StringLength(50)]
     public string Name { get; set; } = String.Empty;
+    [Required]
+    [StringLength(50)]
     public string Genre { get; set; } = String.Empty;
+    [Range(0, 100)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+    [Url]
     public string ImageUrl { get; set; } = String.Empty;
 }
 public record UpdateGameDto
@@ -30,3 +37,4 @@ public record UpdateGameDto
     public DateTime ReleaseDate { get; set; }
     public string ImageUrl { get; set; } = String.Empty;
 }
+
