ff7798924e33e3812eac085169552e92fbc3b897
Abdelrhman-Dev <58493502+AbdrhmanDev@users.noreply.github.com>
2026-05-08 18:50:22 +0300

    Ef core

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ca7132c137a9baba1a7c4b647e7b3c7a6b4133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c8beb1ebc57f2470054a100169ba104dd15694")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
